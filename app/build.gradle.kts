752f80cea0c1ea957c270ad3aa9f36feafc0792d
YuvalThePro <ifraimovy22@gmail.com>
2026-07-02 02:02:30 +0300

    feat: Add Google Services plugin and Firebase BOM dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,7 +43,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
-
+    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.androidx.recyclerview)
